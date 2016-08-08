60695a28a1246edafc23d5d22564bdeca6259c51
dixanta <dixanta@gmail.com>
2016-08-08 18:56:01 +0545

    deprecated removed

--- application/config/config.php
+++ application/config/config.php
@@ -228,7 +228,7 @@ $config['cache_path'] = '';
 | MUST set an encryption key.  See the user guide for info.
 |
 */
-$config['encryption_key'] = "!@#$%^&*_)(*&^%!@#$%^&*asdfghjk!@#$%^&*";
+$config['encryption_key'] = "!@#76!@#$%^&098765!@#$%^&*098765asdfghjk!@#$%^&*kjhgf!@#$%^&*(";
 
 /*
 |--------------------------------------------------------------------------
